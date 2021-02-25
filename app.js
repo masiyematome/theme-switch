d7e1b0b41f8a7315b946e909fbf54717ef26133c
masiyematome <matomemasiye7@gmail.com>
2021-02-25 19:02:10 +0200

    Code that stores a boolen value of the button I clicked

--- app.js
+++ app.js
@@ -14,14 +14,66 @@ buttonsContainer.addEventListener("click" , (event)=> {
     if(clickedButton.classList.contains("dark-theme")){
         clickedButton.classList.add("clicked");
         document.body.classList.add("dark-background");
+
+        const lightTheme = false;
+
+        saveCurrentStateToStorage(lightTheme);
+
     }
 
     else if(clickedButton.classList.contains("light-theme")){
         clickedButton.classList.add("clicked");
         document.body.classList.add("light-background");
 
+        const lightTheme = true;
+
+        saveCurrentStateToStorage(lightTheme);
+
     }
 })
 
 
+function saveCurrentStateToStorage(state){
 
+    let states;
+
+    if(localStorage.getItem("states") === null){
+        states = [];
+    }
+
+    else {
+        states = JSON.parse(localStorage.getItem("states"));
+    }
+
+    states.push(state);
+    localStorage.setItem("states",JSON.stringify(states));
+
+}
+
+(function retrieveSateFromStorage(state){
+
+    let states;
+
+    if(localStorage.getItem("states") === null){
+        states = [];
+    }
+
+    else {
+        states = JSON.parse(localStorage.getItem("states"));
+    }
+
+    states.forEach(function(state){
+
+        // document.body.classList.add("light-background");
+
+        if(state){
+            console.log("I am a light theme");
+        }
+
+        else if(!state){
+            console.log("I am a dark theme");
+        }
+        
+    })
+
+}());
